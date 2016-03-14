41b9ffcb909b6ebda5737eed7d65b4de67f1d3f2
Wing Lung Ngai <winglung.ngai@gmail.com>
2016-03-14 11:20:27 +0100

    clean

--- granula-archiver/build.sh
+++
@@ -1,6 +0,0 @@
-mvn -DskipTests clean package
-
-version=0.1.1
-mvn install:install-file -Dfile=target/granula-archiver-$version.jar -DpomFile=pom.xml -Dsources=target/granula-archiver-$version-sources.jar -DgroupId=nl.tudelft.pds.granula -DartifactId=granula-archiver -Dversion=$version -Dpackaging=jar
-
-
